835ab183ecc646e8d21b6988d3d565690ffa0f7d
ravit <ravit@TEJARAVI>
2024-03-16 22:01:09 -0500

    on 3/16/2024: Updated the framework and prepared the test data on Json file, send it to Fillform test case.

--- Fillform_ecommerce_C#/Tests/UnitTest1.cs
+++ Fillform_ecommerce_C#/Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Fillform_ecommerce_C.Pageobjects;
+using Fillform_ecommerce_C.utilities;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -6,65 +8,60 @@ using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace Fillform_ecommerce_C_
+namespace Fillform_ecommerce_C.Tests
 {
-    public class E2ETest
+    public class E2ETest : Base
     {
-        public IWebDriver driver;
-        String[] expectedProducts = { "iphone X", "Blackberry" };
-        String[] actualProducts = new string[2];
-        [SetUp]
-        public void Startbrowser()
-        {
-            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
 
-            driver.Manage().Window.Maximize();
+        string[] expectedProducts = { "iphone X", "Blackberry" };
+        string[] actualProducts = new string[2];
 
-            driver.Url = "https://rahulshettyacademy.com/angularpractice/";
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
-        }
-
-        [Test]
-        public void FormSubmission()
+        [Test, TestCaseSource("Addtestdataconfig")]
+        public void Test_FormSubmission(string name, string email, string password)
         {
 
-            driver.FindElement(By.Name("name")).SendKeys("Ravi Teja");
-            driver.FindElement(By.XPath("//input [@name='email']")).SendKeys("ravitejap976@gmail.com");
-            driver.FindElement(By.Id("exampleInputPassword1")).SendKeys("Ravi@12");
-            driver.FindElement(By.CssSelector("input[type ='checkbox']")).Click();
-
-            IWebElement dropdown = driver.FindElement(By.XPath("//select [@id='exampleFormControlSelect1']"));
-
-            SelectElement S = new SelectElement(dropdown);
+            FormPage formpage = new FormPage(getdriver());
+            formpage.getName().SendKeys(name);
+            formpage.getEmail().SendKeys(email);
+            formpage.getPassword().SendKeys(password);
+            formpage.getCheckbox().Click();
+            IWebElement drop = formpage.getDropdown();
+            SelectElement S = new SelectElement(drop);
             S.SelectByText("Female");
 
-            IList <IWebElement> rados = driver.FindElements(By.CssSelector("input[type='radio']"));
+            IList<IWebElement> rads = formpage.getRados();
 
-            foreach (IWebElement rad in rados)
+            foreach (IWebElement rad in rads)
             {
                 if (rad.GetAttribute("value").Equals("option1"))
                 {
                     rad.Click();
                 }
             }
-            driver.FindElement(By.CssSelector("input[value='Submit']")).Click();
+            formpage.getSubmitbutton().Click();
 
             //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(".alert-success")));
 
-            String confirText = driver.FindElement(By.CssSelector(".alert-success")).Text;
+            string confirText = formpage.getMessage().Text;
 
             StringAssert.Contains("Success", confirText);
 
             Console.WriteLine(confirText);
-          
+        }
+
+        public static IEnumerable<TestCaseData> Addtestdataconfig()
+        {
+            yield return new TestCaseData(getDataReader().ReadsData("name"), getDataReader().ReadsData("email"), getDataReader().ReadsData("password"));
+            Console.WriteLine("Json Dataset passed to testcase");
+            yield return new TestCaseData(getDataReader().ReadsData("name1"), getDataReader().ReadsData("email1"), getDataReader().ReadsData("password"));
+            Console.WriteLine("Json Dataset2 passed to testcase");
+            yield return new TestCaseData("Teja", "ravitejap6@gmail.com", "Ravi@1");
+            Console.WriteLine("String Dataset passed to testcase");
         }
 
         [Test]
-        public void ClicktoOpenShopTab() 
+        public void ClicktoOpenShopTab()
         {
 
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
@@ -74,15 +71,15 @@ namespace Fillform_ecommerce_C_
 
             Console.WriteLine("Successfully clicked Shop tab");
             wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.PartialLinkText("Checkout")));
-            
+
             SelectionProducts();
         }
 
-          
+
         public void SelectionProducts()
         {
-            
-            IList <IWebElement> products = driver.FindElements(By.TagName("app-card"));
+
+            IList<IWebElement> products = driver.FindElements(By.TagName("app-card"));
 
             foreach (IWebElement product in products)
             {
@@ -95,41 +92,34 @@ namespace Fillform_ecommerce_C_
 
             driver.FindElement(By.ClassName("btn-primary")).Click();
 
-            WebDriverWait wait1 = new WebDriverWait(driver,TimeSpan.FromSeconds(5));
+            WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             wait1.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.ClassName("btn-success")));
 
             driver.FindElement(By.ClassName("btn-success")).Click();
 
             //dyanamic dropdown 
-      
+
             driver.FindElement(By.Id("country")).SendKeys("fin");
 
             IList<IWebElement> Options = driver.FindElements(By.CssSelector("app-checkout[class='row'] ul"));
-            
+
             foreach (IWebElement option in Options)
             {
                 if (option.Text.Equals("Finland"))
                 {
                     option.Click();
                 }
-             
+
             }
             TestContext.Progress.WriteLine(driver.FindElement(By.Id("country")).GetAttribute("value"));
             driver.FindElement(By.CssSelector("label[for*='checkbox2']")).Click();
             driver.FindElement(By.CssSelector("input[value='Purchase']")).Click();
-            String S = driver.FindElement(By.ClassName("alert-success")).Text;
+            string S = driver.FindElement(By.ClassName("alert-success")).Text;
 
             StringAssert.Contains("Success", S);
             TestContext.Progress.WriteLine(S);
 
             Console.WriteLine("Successfully Placed order using C#");
-        }
-
-        
-        [TearDown]
-        public void Browserclose()
-        {
-            driver.Close();
         }
     }
 }
